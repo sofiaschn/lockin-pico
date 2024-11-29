6cf02664862d5d72c10e7101f2a11b1eb6c05395
Sofia Schneider Camilo <sofia.schn.c@gmail.com>
2024-11-28 21:57:29 -0300

    Add PWM generation code

--- lockin-pico.c
+++ lockin-pico.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
+#include <math.h>
 #include "pico/stdlib.h"
+#include "hardware/pwm.h"
 
+#define CLOCK_FREQ 125000000
 
+#define PWM_PIN 1
+#define PWM_FREQ 500
+#define DUTY_CYCLE_PERCENT 50
+
+// For an explanation in how the PWM works, visit the URL below
+// https://www.i-programmer.info/programming/hardware/14849-the-pico-in-c-basic-pwm.html?start=1
+void init_pwm() {
+    // Allocate gpio 1 to PWM
+    gpio_set_function(PWM_PIN, GPIO_FUNC_PWM);
+
+    // Find out which PWM slice and channel is connected to GPIO 1
+    uint slice_num = pwm_gpio_to_slice_num(PWM_PIN);
+    uint channel = pwm_gpio_to_channel(PWM_PIN);
+
+    // Calculate how much we need to divide the clock frequency
+    float clock_divider = ceil(CLOCK_FREQ / (4096 * PWM_FREQ)) / 16;
+    pwm_set_clkdiv(slice_num, clock_divider);
+
+    // Calculate the wrap value of the PWM counter (how much it counts before resetting)
+    float divided_clock_freq = CLOCK_FREQ / clock_divider;
+    uint16_t counter_wrap = (divided_clock_freq / PWM_FREQ) - 1;
+    pwm_set_wrap(slice_num, counter_wrap);
+
+    // Calculate the level (count value) at which the PWM should switch between 1 and 0
+    uint16_t level = counter_wrap / (100 / DUTY_CYCLE_PERCENT);
+    pwm_set_chan_level(slice_num, channel, level);
+
+    // Enable PWM
+    pwm_set_enabled(slice_num, true);
+}
 
 int main()
 {
     stdio_init_all();
+
+    init_pwm();
 
     while (true) {
         printf("Hello, world!\n");
