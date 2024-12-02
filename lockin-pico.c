c04cb7d006d3bc6fe8051f4dacc015151743c6aa
Sofia Schneider <sofia.schn.c@gmail.com>
2024-12-01 22:50:44 -0300

    Configure DMA for the ADC

--- lockin-pico.c
+++ lockin-pico.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 #include "hardware/clocks.h"
 #include "hardware/adc.h"
+#include "hardware/dma.h"
 
 #define CLOCK_FREQ_HZ 270000000
+
+// ADC frequencies over 135 MHz showed distortions around the 2048 mark (half of the 12-bit range)
+#define ADC_FREQ_DIVIDER 2
+#define ADC_FREQ_HZ (CLOCK_FREQ_HZ / ADC_FREQ_DIVIDER)
+
+#define DMA_CHANNEL 0
 
 #define PWM_PIN 1
 #define PWM_FREQ 500
 #define DUTY_CYCLE_PERCENT 50
 
-#define ADC_PIN 27
+#define REFERENCE_ADC_PIN 27
+#define INPUT_ADC_PIN 28
+
+uint adc_capture_buffer_size = ((4.0 * 1000000 / PWM_FREQ) / (96.0 * 1000000 / ADC_FREQ_HZ)) + 1;
+uint16_t* adc_capture_buffer;
 
 // For an explanation in how the PWM works, visit the URL below
 // https://www.i-programmer.info/programming/hardware/14849-the-pico-in-c-basic-pwm.html?start=1
@@ -41,72 +53,68 @@ void init_pwm() {
     pwm_set_enabled(slice_num, true);
 }
 
-void init_adc() {
+bool init_adc() {
     adc_init();
     
-    adc_gpio_init(ADC_PIN);
+    adc_gpio_init(REFERENCE_ADC_PIN);
+    adc_gpio_init(INPUT_ADC_PIN);
 
     // Set the ADC clock source register to use the system clock
-    // ADC frequencies over 125 MHz showed distortions around the 2048 mark (half of the 12-bit range)
-    uint adc_frequency_divisor = 2;
-    clock_configure(clk_adc, 0, CLOCKS_CLK_ADC_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, CLOCK_FREQ_HZ, CLOCK_FREQ_HZ / adc_frequency_divisor);
+    clock_configure(clk_adc, 0, CLOCKS_CLK_ADC_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, CLOCK_FREQ_HZ, ADC_FREQ_HZ);
+
+    // Set up the ADC FIFO to write every sample to the FIFO, call the DMA interrupt every sample,
+    // disable the error bit and maintain 12-bit samples
+    adc_fifo_setup(true, true, 1, false, false);
+
+    // Get DMA channel and default config
+    dma_channel_claim(DMA_CHANNEL);
+    dma_channel_start(DMA_CHANNEL);
+    dma_channel_config cfg = dma_channel_get_default_config(DMA_CHANNEL);
+
+    // Reading from constant address, writing to incrementing byte addresses, transferring 16 bits
+    channel_config_set_transfer_data_size(&cfg, DMA_SIZE_16);
+    channel_config_set_read_increment(&cfg, false);
+    channel_config_set_write_increment(&cfg, true);
+
+    // Pace transfers based on availability of ADC samples
+    channel_config_set_dreq(&cfg, DREQ_ADC);
+    
+    // Allocate the buffer on memory
+    adc_capture_buffer = malloc(adc_capture_buffer_size * sizeof(uint16_t));
+    if (adc_capture_buffer == NULL) {
+        printf("ERROR WHILE ALLOCATING MEMORY FOR CAPTURE_BUFFER!\n");
+
+        return false;
+    }
+
+    // Configure the DMA channel to read from ADC FIFO and write to capture buffer
+    dma_channel_configure(DMA_CHANNEL, &cfg, adc_capture_buffer, &adc_hw->fifo, adc_capture_buffer_size, false);
 }
 
-uint16_t read_adc_raw(uint gpio) {
+void start_adc_sampling(uint gpio) {
     // ADC inputs are from 0-3 (GPIO 26-29)
     adc_select_input(gpio - 26);
 
-    return adc_read();
+    // Reset the write address back to the start of the capture buffer
+    dma_channel_set_write_addr(DMA_CHANNEL, adc_capture_buffer, true);
+
+    // Start free-running sampling mode
+    adc_run(true);
+
+    // Once DMA finishes, stop any new conversions from starting, and clean up
+    // the FIFO in case the ADC was still mid-conversion
+    dma_channel_wait_for_finish_blocking(DMA_CHANNEL);
+    adc_run(false);
+    adc_fifo_drain();
 }
 
-float read_adc_voltage(uint gpio) {
-    // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
-    const float conversion_factor = 3.3f / (1 << 12);
+uint16_t get_average_adc(uint gpio) {
+    start_adc_sampling(gpio);
 
-    uint16_t adc_raw_value = read_adc_raw(gpio);
-
-    return adc_raw_value * conversion_factor;
-}
-
-// Only needed for the systick timer
-#include "hardware/structs/systick.h"
-
-void measure_avg_adc_time_us() {
-    // Register magic to setup the systick timer
-    systick_hw->csr = 0x5;
-    systick_hw->rvr = 0x00FFFFFF;
-
-    #define MAX_ITER 1200
-
-    float arr[MAX_ITER];
     uint accumulator = 0;
+    for (int i = 0; i < adc_capture_buffer_size; i++) accumulator += adc_capture_buffer[i];
 
-    for (uint i = 0; i < MAX_ITER; i++) {
-        // Get the start tick
-        uint32_t start_tick = systick_hw->cvr;
-
-        arr[i] = read_adc_voltage(ADC_PIN);
-
-        // Get the end tick and the difference
-        uint32_t end_tick = systick_hw->cvr;
-        uint32_t number_of_ticks = start_tick - end_tick;
-
-        accumulator += number_of_ticks;
-    }
-
-    // Get the time in microseconds
-    float result = (accumulator / MAX_ITER) * (1000000.0 / CLOCK_FREQ_HZ);
-    printf("Tempo médio da medida do ADC: %f us\n", result);
-
-    // Prints the values
-    /*printf("[");
-    for (int i = 0; i < MAX_ITER; i++) {
-        if ((i + 1) == MAX_ITER) {
-            printf("%f]\n\n\n", arr[i]);
-        } else {
-            printf("%f, ", arr[i]);
-        }
-    }*/
+    return round(accumulator / adc_capture_buffer_size);
 }
 
 int main()
@@ -118,10 +126,13 @@ int main()
     stdio_init_all();
 
     init_pwm();
-    init_adc();
+
+    bool success = init_adc();
+    if (!success) return 1;
 
     while (true) {
-        measure_avg_adc_time_us();
-        sleep_ms(5000);
+        uint16_t average_ref = get_average_adc(REFERENCE_ADC_PIN);
+        printf("Tensão média da referência: %f\n", average_ref * 3.3f / (1 << 12));
+        sleep_ms(1000);
     }
 }
